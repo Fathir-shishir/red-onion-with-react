e7a74b81f9c4625c16f7d3b936a6fcacf7289ccc
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-04-15 17:04:12 +0200

    header added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo2.png'
+
+const Header = () => {
+    return (
+        <div className='d-flex align-items-center container mt-2'>
+        <div className="col-md-6 ">
+            <img style={{height:"40px",width:"200px"}} src={logo} alt="" />
+            
+        </div>
+        <div className="col-md-6 d-flex justify-content-end" >
+            <nav>
+            <Link className='mx-3 text-decoration-none text-black' to="/about">Log In</Link>
+            <Link className='mx-3  text-decoration-none text-black' to="/about">Sing up</Link>
+            </nav>
+        </div>
+            
+        </div>
+    );
+};
+
+export default Header;
